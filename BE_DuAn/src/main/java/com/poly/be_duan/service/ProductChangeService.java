2f5d9aa52c55341d20ffabb15732f24f1c948d80
Quantran5302 <quantran939@gmail.com>
2023-09-06 08:49:57 +0700

    first commit

---
+++ BE_DuAn/src/main/java/com/poly/be_duan/service/ProductChangeService.java
@@ -0,0 +1,18 @@
+package com.poly.be_duan.service;
+
+import com.poly.be_duan.entities.ProductChange;
+
+import java.util.List;
+
+public interface ProductChangeService extends GenericService<ProductChange, Integer> {
+    public List<ProductChange> listProductChange();
+    public List<ProductChange> findAllProductChange();
+    public List<ProductChange> findProductChangeByStatus(Integer status,String phone);
+    ProductChange findByStatus(Integer idPrChange);
+    public List<ProductChange> findByUsername(String username);
+    public List<ProductChange> findByStatusSendEmail(Integer idChange);
+
+    public Integer sumStatus(String number);
+    public ProductChange getByIDbILLdt(Integer id);
+
+}
